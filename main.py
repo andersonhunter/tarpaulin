f9fa957d18d3dd5293dbd4a4b0d98f7d9529e4af
Hunter Anderson <129843902+andersonhunter@users.noreply.github.com>
2025-05-27 19:03:03 +0000

    Added auth0 code

--- main.py
+++ main.py
@@ -77,6 +77,76 @@ def get_token_auth_header():
     return token
 
 
+def requires_auth(f):
+    """
+    Determines if the access token is valid.
+    Adapted from: https://auth0.com/docs/quickstart/backend/python
+    """
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = get_token_auth_header()
+        jsonurl = urlopen("https://" + AUTH0_DOMAIN + "/.well-known/jwks.json")
+        unverified_header = jwt.get_unverified_header(token)
+        public_key = None
+        for key in jwks["keys"]:
+            if key["kid"] == unverified_header["kid"]:
+                public_key = jwt.algorithms.RSAAlgorithm.from_jwk(json.dumps(jwk))
+        if public_key:
+            try:
+                payload = jwt.decode(
+                    token,
+                    public_key,
+                    algorithms=ALGORITHMS,
+                    audience=API_AUDIENCE,
+                    issuer="https://" + AUTH0_DOMAIN + "/"
+                    )
+            except jwt.ExpiredSignatureError:
+                raise AuthError({
+                    "code": "token_expired",
+                    "description": "token is expired"
+                }, 401)
+            except jwt.InvalidAudienceError:
+                raise AuthError({
+                    "code": "invalid_audience",
+                    "description": "incorrect audience, please check audience"
+                }, 401)
+            except jwt.InvalidIssuerError:
+                raise AuthError({
+                    "code": "invalid_issuer",
+                    "description": "incorrect issuer, please check the issuer"
+                }, 401)
+            except Exception:
+                raise AuthError({
+                    "code": "invalid_header",
+                    "description": "Unable to parse auth token"
+                }, 401)
+            _request_ctx_stack.top_current_user = payload
+            return f(*args, **kwargs)
+        raise AuthError({
+            "code": "invalid_header",
+            "description": "Unable to find appropriate key"
+        }, 401)
+    return decorated
+
+
+def requires_scope(required_scope):
+    """
+    Determines if the required scope is present in the access token
+    Args: 
+        required_scope(str): The scope required to access the resource
+    Adapted from: https://auth0.com/docs/quickstart/backend/python
+    """
+    token = get_token_auth_header()
+    unverified_claims = jwt.decode(token, options={"verify_signature": False})
+    if unverified_claims.get("scope"):
+        token_scopes = unverified_claims["scope"].split()
+        for token_scope in token_scopes:
+            if token_scope == required_scope:
+                return True
+    return False
+
+
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
 
